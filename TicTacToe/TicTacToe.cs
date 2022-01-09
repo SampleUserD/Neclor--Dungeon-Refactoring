8b036b0839417a58deb35f364e4812a49a293065
neclor <al.pavlov2012@gmail.com>
2022-01-09 21:32:36 +0500

    обработка неправильных нажатий убран лишний цикл

--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -15,32 +15,23 @@
         Console.SetCursorPosition(0, 11);
         Console.Write("                       ");
         Console.SetCursorPosition(0, 11);
-        if (m[1] == m[2] && m[2] == m[3] && m[1] != " " || m[1] == m[4] && m[4] == m[7] && m[1] != " ")
-        {
+        if (m[1] == m[2] && m[2] == m[3] && m[1] != " " || m[1] == m[4] && m[4] == m[7] && m[1] != " ") {
             Console.Write("Победили:" + m[1]);
             return 1;
         }
-
-        else if (m[4] == m[5] && m[5] == m[6] && m[5] != " " || m[2] == m[5] && m[5] == m[8] && m[5] != " " || m[1] == m[5] && m[5] == m[9] && m[5] != " " || m[7] == m[5] && m[5] == m[3] && m[5] != " ")
-        {
+        else if (m[4] == m[5] && m[5] == m[6] && m[5] != " " || m[2] == m[5] && m[5] == m[8] && m[5] != " " || m[1] == m[5] && m[5] == m[9] && m[5] != " " || m[7] == m[5] && m[5] == m[3] && m[5] != " ") {
             Console.Write("Победили:" + m[5]);
             return 1;
         }
-
-        else if (m[7] == m[8] && m[8] == m[9] && m[9] != " " || m[3] == m[6] && m[6] == m[9] && m[9] != " ")
-        {
+        else if (m[7] == m[8] && m[8] == m[9] && m[9] != " " || m[3] == m[6] && m[6] == m[9] && m[9] != " ") {
             Console.Write("Победили:" + m[9]);
             return 1;
         }
-
-        else if (!m.Contains(" "))
-        {
+        else if (!m.Contains(" ")) {
             Console.Write("Ничья");
             return 1;
         }
-
-        else
-        {
+        else {
             return 0;
         }
     }
@@ -71,8 +62,7 @@
         Console.SetCursorPosition(0, 11);
         Console.Write("Нажмите X или O:");
         string p;
-        switch (char.ToUpper(Console.ReadKey().KeyChar))
-        {
+        switch (char.ToUpper(Console.ReadKey().KeyChar)) {
             case 'X':
                 p = "X";
                 break;
@@ -85,40 +75,40 @@
 
 
 
-        if (p == "O")
-        {
+        if (p == "O") {
             i = r.Next(1, 9);
             WriteXO(i, "X");
             m[i] = "X";
         }
 
-        while (true)
-        {
-            while (true)
-            {
-                Console.SetCursorPosition(0, 11);
-                Console.Write("                       ");
-                Console.SetCursorPosition(0, 11);
-                Console.Write("Введите сектор:");
+        while (true) {
+            
+            Console.SetCursorPosition(0, 11);
+            Console.Write("                       ");
+            Console.SetCursorPosition(0, 11);
+            Console.Write("Введите сектор 1..9:");
+
+            try {
                 i = Convert.ToInt32(Console.ReadKey().KeyChar.ToString());
-                if (m[i] == " ")
-                {
-                    WriteXO(i, p);
-                    m[i] = p;
-                    break;
-                }
+            }
+            catch (FormatException) {
+                continue;
             }
 
-            if (Check() == 1)
-            {
+
+            if (m[i] != " ")
+                continue;
+
+            WriteXO(i, p);
+            m[i] = p;            
+
+            if (Check() == 1) {
                 break;
             }
 
-            while (true)
-            {
+            while (true) {
                 i = r.Next(1, 9);
-                if (m[i] == " ")
-                {
+                if (m[i] == " ") {
                     string c = p == "X" ? (c = "O") : (c = "X");
                     m[i] = c;
                     WriteXO(i, c);
@@ -126,11 +116,11 @@
                 }
             }
 
-            if (Check() == 1)
-            {
+            if (Check() == 1) {
                 break;
             }
-
         }
     }
 }
+
+
