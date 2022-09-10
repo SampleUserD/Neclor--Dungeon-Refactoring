967f93325ac662c1d2f2ac23061fae772ee9c665
SampleUserD <61961965+SampleUserD@users.noreply.github.com>
2022-09-10 16:21:24 +0500

    Refactoring #1 (Better names)
    There is nothing that affects public API of the class, but internals

--- sources/Enemy.cs
+++ sources/Enemy.cs
@@ -1,44 +1,115 @@
 ﻿namespace Dungeon
 {
+    /**
+     * @description
+     * This class defines an easy interface to the vector in 2d space
+     * 
+     * @param {double} x - coordinate by X
+     * @param {double} y - coordinate by Y
+     */
+    public record Vector(double X, double Y);
+
     public class Enemy
     {
-        private Double x, y;
+        private readonly const double OneHalfOfTheStep = 0.5;
 
+        private Vector _position = default(Vector);
+
+        /**
+         * @description
+         * This class defines an interface of the enemy
+         * 
+         * @param {int} x - position of the enemy by x
+         * @param {int} y - position of the enemy by y
+         */
         public Enemy(int x, int y)
         {
-            this.x = x;
-            this.y = y;
+            _position = new Vector(x, y);
         }
 
+        /**
+         * @description
+         * There is no Point in parameters, because it is expensive to recreate object every cycle
+         * That is why there is inconsistence in code
+         * 
+         * @param {int} px - Player position by X
+         * @param {int} py - Player position by Y
+         * 
+         * @returns {bool}
+         */
+        private bool IsPlayerStaysAtNearbyPosition(int px, int py)
+        {
+            int x = Convert.ToInt32(_position.X);
+            int y = Convert.ToInt32(_position.Y);
+
+            return Math.Abs(px - x) == 0 &&
+                   Math.Abs(py - y) == 1 ||
+                   Math.Abs(px - x) == 1 &&
+                   Math.Abs(py - y) == 0;
+        }
+
+        /**
+         * @description
+         * There is no Point in parameters, because it is expensive to recreate object every cycle
+         * That is why there is inconsistence in code
+         * Enemy chases player every second frame
+         * 
+         * @param {int} px - Player position by X
+         * @param {int} py - Player position by Y
+         * 
+         * @returns {void}
+         */
+        private void ChaseThePlayer(int px, int py)
+        {
+            int x = Convert.ToInt32(_position.X);
+            int y = Convert.ToInt32(_position.Y);
+
+            if (px > x)
+            {
+                _position.X += OneHalfOfTheStep;
+            }
+            else if (px < x)
+            {
+                _position.X -= OneHalfOfTheStep;
+            }
+            else if (py > y)
+            {
+                _position.Y += OneHalfOfTheStep;
+            }
+            else if (py < y)
+            {
+                _position.Y -= OneHalfOfTheStep;
+            }
+        }
+
+        /**
+         * @description
+         * 1) This method defines a moving strategy
+         * 2) Renders a enemy on the screen
+         * 
+         * @returns {void}
+         */
         public void Moving()
         {
-            if (Math.Abs(Dungeon.MyX - Convert.ToInt32(x)) == 0 && Math.Abs(Dungeon.MyY - Convert.ToInt32(y)) == 1 || Math.Abs(Dungeon.MyX - Convert.ToInt32(x)) == 1 && Math.Abs(Dungeon.MyY - Convert.ToInt32(y)) == 0) 
+            int x = Convert.ToInt32(_position.X);
+            int y = Convery.ToInt32(_position.Y);
+
+            int playerX = Dungeon.MyX;
+            int playerY = Dungeon.MyY;
+
+            if (IsPlayerStaysAtNearbyPosition(playerX, playerY) == true)
             {
                 Dungeon.Lose();
+                return void;
             }
 
-            Console.SetCursorPosition(Convert.ToInt32(x), Convert.ToInt32(y));
+            Console.SetCursorPosition(x, y);
             Console.WriteLine(" ");
             Console.SetCursorPosition(0, 25);
 
-            if (Dungeon.MyX > Convert.ToInt32(x)) 
-            {
-                x += 0.5;
-            }
-            else if (Dungeon.MyX < Convert.ToInt32(x)) 
-            {
-                x -= 0.5;
-            }
-            else if (Dungeon.MyY > Convert.ToInt32(y)) 
-            {
-                y += 0.5;
-            }
-            else if (Dungeon.MyY < Convert.ToInt32(y)) 
-            {
-                y -= 0.5;
-            }
+            ChaseThePlayer(playerX, playerY);
 
-            Console.SetCursorPosition(Convert.ToInt32(x), Convert.ToInt32(y));
+            Console.SetCursorPosition(x, y);
             Console.WriteLine("&");
             Console.SetCursorPosition(0, 25);
         }
