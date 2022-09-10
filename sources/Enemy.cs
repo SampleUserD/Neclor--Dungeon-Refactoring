0216fa0f4adb18c359c9047e5349208fa0e21f31
neclor <81529592+neclor@users.noreply.github.com>
2022-09-10 19:17:38 +0500

    Update Enemy.cs
    Refactor

--- sources/Enemy.cs
+++ sources/Enemy.cs
@@ -1,117 +1,110 @@
-﻿namespace Dungeon
+namespace Dungeon;
+
+/**
+ * @description
+ * This class defines an easy interface to the vector in 2d space
+ * 
+ * @param {double} x - coordinate by X
+ * @param {double} y - coordinate by Y
+ */
+public record Vector(double X, double Y);
+
+public class Enemy
 {
+    private const double OneHalfOfTheStep = 0.5;
+
+    private Vector _position = default(Vector);
+
     /**
      * @description
-     * This class defines an easy interface to the vector in 2d space
+     * This class defines an interface of the enemy
      * 
-     * @param {double} x - coordinate by X
-     * @param {double} y - coordinate by Y
+     * @param {int} x - position of the enemy by x
+     * @param {int} y - position of the enemy by y
      */
-    public record Vector(double X, double Y);
-
-    public class Enemy
+    public Enemy(int x, int y)
     {
-        private readonly const double OneHalfOfTheStep = 0.5;
+        _position = new Vector(x, y);
+    }
 
-        private Vector _position = default(Vector);
+    /**
+     * @description
+     * There is no Point in parameters, because it is expensive to recreate object every cycle
+     * That is why there is inconsistence in code
+     * 
+     * @param {int} px - Player position by X
+     * @param {int} py - Player position by Y
+     * 
+     * @returns {bool}
+     */
+    private bool IsPlayerStaysAtNearbyPosition(int px, int py)
+    {
+        int x = Convert.ToInt32(_position.X);
+        int y = Convert.ToInt32(_position.Y);
 
-        /**
-         * @description
-         * This class defines an interface of the enemy
-         * 
-         * @param {int} x - position of the enemy by x
-         * @param {int} y - position of the enemy by y
-         */
-        public Enemy(int x, int y)
-        {
-            _position = new Vector(x, y);
+        return Math.Abs(px - x) == 0 &&
+               Math.Abs(py - y) == 1 ||
+               Math.Abs(px - x) == 1 &&
+               Math.Abs(py - y) == 0;
+    }
+
+    /**
+     * @description
+     * There is no Point in parameters, because it is expensive to recreate object every cycle
+     * That is why there is inconsistence in code
+     * Enemy chases player every second frame
+     * 
+     * @param {int} px - Player position by X
+     * @param {int} py - Player position by Y
+     * 
+     * @returns {void}
+     */
+    private void ChaseThePlayer(int px, int py)
+    {
+        int x = Convert.ToInt32(_position.X);
+        int y = Convert.ToInt32(_position.Y);
+
+        if (px > x) {
+            _position.X += OneHalfOfTheStep;
+        }
+        else if (px < x) {
+            _position.X -= OneHalfOfTheStep;
+        }
+        else if (py > y) {
+            _position.Y += OneHalfOfTheStep;
+        }
+        else if (py < y) {
+            _position.Y -= OneHalfOfTheStep;
+        }
+    }
+
+    /**
+     * @description
+     * 1) This method defines a moving strategy
+     * 2) Renders a enemy on the screen
+     * 
+     * @returns {void}
+     */
+    public void Moving()
+    {
+        int x = Convert.ToInt32(_position.X);
+        int y = Convert.ToInt32(_position.Y);
+
+        int playerX = Dungeon.MyX;
+        int playerY = Dungeon.MyY;
+
+        if (IsPlayerStaysAtNearbyPosition(playerX, playerY) == true) {
+            Dungeon.Lose();
         }
 
-        /**
-         * @description
-         * There is no Point in parameters, because it is expensive to recreate object every cycle
-         * That is why there is inconsistence in code
-         * 
-         * @param {int} px - Player position by X
-         * @param {int} py - Player position by Y
-         * 
-         * @returns {bool}
-         */
-        private bool IsPlayerStaysAtNearbyPosition(int px, int py)
-        {
-            int x = Convert.ToInt32(_position.X);
-            int y = Convert.ToInt32(_position.Y);
+        Console.SetCursorPosition(x, y);
+        Console.WriteLine(" ");
+        Console.SetCursorPosition(0, 25);
 
-            return Math.Abs(px - x) == 0 &&
-                   Math.Abs(py - y) == 1 ||
-                   Math.Abs(px - x) == 1 &&
-                   Math.Abs(py - y) == 0;
-        }
+        ChaseThePlayer(playerX, playerY);
 
-        /**
-         * @description
-         * There is no Point in parameters, because it is expensive to recreate object every cycle
-         * That is why there is inconsistence in code
-         * Enemy chases player every second frame
-         * 
-         * @param {int} px - Player position by X
-         * @param {int} py - Player position by Y
-         * 
-         * @returns {void}
-         */
-        private void ChaseThePlayer(int px, int py)
-        {
-            int x = Convert.ToInt32(_position.X);
-            int y = Convert.ToInt32(_position.Y);
-
-            if (px > x)
-            {
-                _position.X += OneHalfOfTheStep;
-            }
-            else if (px < x)
-            {
-                _position.X -= OneHalfOfTheStep;
-            }
-            else if (py > y)
-            {
-                _position.Y += OneHalfOfTheStep;
-            }
-            else if (py < y)
-            {
-                _position.Y -= OneHalfOfTheStep;
-            }
-        }
-
-        /**
-         * @description
-         * 1) This method defines a moving strategy
-         * 2) Renders a enemy on the screen
-         * 
-         * @returns {void}
-         */
-        public void Moving()
-        {
-            int x = Convert.ToInt32(_position.X);
-            int y = Convery.ToInt32(_position.Y);
-
-            int playerX = Dungeon.MyX;
-            int playerY = Dungeon.MyY;
-
-            if (IsPlayerStaysAtNearbyPosition(playerX, playerY) == true)
-            {
-                Dungeon.Lose();
-                return void;
-            }
-
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine(" ");
-            Console.SetCursorPosition(0, 25);
-
-            ChaseThePlayer(playerX, playerY);
-
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine("&");
-            Console.SetCursorPosition(0, 25);
-        }
+        Console.SetCursorPosition(x, y);
+        Console.WriteLine("&");
+        Console.SetCursorPosition(0, 25);
     }
 }
