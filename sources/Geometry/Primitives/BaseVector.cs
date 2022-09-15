240621e39c7e4820ceae7a5d6c551b4133c6569b
SampleUserD <61961965+SampleUserD@users.noreply.github.com>
2022-09-15 20:15:54 +0500

    Refactoring #13 (DRY #2)
    Added abstract record BaseVector(X, Y)

---
+++ sources/Geometry/Primitives/BaseVector.cs
@@ -0,0 +1,26 @@
+﻿namespace Dungeon.Geometry.Primitives;
+
+/// <summary>
+/// Presents standard operations which are common for all types of vectors
+/// </summary>
+/// <param name="X">Coordinate by X-axis</param>
+/// <param name="Y">Coordinate by Y-axis</param>
+public abstract record BaseVector(double X, double Y)
+{
+    public double Length() => Math.Sqrt(X * X + Y * Y);
+    public double Dot(BaseVector other) => this.X * other.X + this.Y * other.Y;
+
+    /// <summary>
+    /// Calculate angle between vector and X-axis as known as direction.
+    /// Angle belongs to interval (0; pi)
+    /// </summary>
+    /// <returns>Angle in radians</returns>
+    public double Direction() => Math.Atan2(this.Y, this.X);
+
+    /// <summary>
+    /// Return angle between current vector and given vector.
+    /// Angle belongs to interval (0; pi)
+    /// </summary>
+    /// <returns>Angle in radians</returns>
+    public double AngleBetween(BaseVector other) => Math.Acos(Dot(other) / (this.Length() * other.Length()));
+}
--- sources/Geometry/Primitives/Vector.cs
+++ sources/Geometry/Primitives/Vector.cs
@@ -1,47 +1,29 @@
 namespace Dungeon.Geometry.Primitives;
-/**
- * @description
- * This class defines an easy interface to the vector in 2d space
- * 
- * @param {double} x - coordinate by X
- * @param {double} y - coordinate by Y
- */
-public record Vector(double X, double Y)
+
+/// <summary>
+/// Represents vector on plane
+/// </summary>
+/// <param name="X">Coordinate by X-axis</param>
+/// <param name="Y">Coordinate by Y-axis</param>
+public record Vector(double X, double Y) : BaseVector(X, Y)
 {
-    public double Length() => Math.Sqrt(X * X + Y * Y);
-    public double Dot(Vector other) => this.X * other.X + this.Y * other.Y;
-
-    /// <summary>
-    /// Calculate angle between vector and X-axis as known as direction.
-    /// Angle belongs to interval (0; pi)
-    /// </summary>
-    /// <returns>Angle in radians</returns>
-    public double Direction() => Math.Atan2(this.Y, this.X);
-
-    /// <summary>
-    /// Return angle between current vector and given vector.
-    /// Angle belongs to interval (0; pi)
-    /// </summary>
-    /// <returns>Angle in radians</returns>
-    public double AngleBetween(Vector other) => Math.Acos(Dot(other) / (this.Length() * other.Length()));
-
     public static Vector operator +(Vector left, Vector right)
     {
-        return new Vector(left.X + right.X, left.Y + right.Y);
+        return new(left.X + right.X, left.Y + right.Y);
     }
 
     public static Vector operator -(Vector left, Vector right)
     {
-        return new Vector(left.X - right.X, left.Y - right.Y);
+        return new(left.X - right.X, left.Y - right.Y);
     }
 
     public static Vector operator -(Vector left)
     {
-        return new Vector(-left.X, -left.Y);
+        return new(-left.X, -left.Y);
     }
 
     public static Vector operator *(double coefficient, Vector right)
     {
-        return new Vector(coefficient * right.X, coefficient * right.Y);
+        return new(coefficient * right.X, coefficient * right.Y);
     }
 }
